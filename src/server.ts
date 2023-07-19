93171dfd91f4f9520933628ec3781efba2197145
CristianoNunes <cristianonunes99@gmail.com>
2023-07-19 00:13:05 -0300

    Add cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import "reflect-metadata";
 import express from "express";
+const cors = require("cors");
 
 import { routes } from "./routes";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
